fd094fb0e333e4adedafbdf76edc51c3f09936d7
William Gadd <wgadd@sasktel.net>
2024-01-19 20:56:22 -0600

    Add tests for when route involves a transfer and test that the shortest route is used when there are multiple options

--- tests/passenger_bus_route.rs
+++ tests/passenger_bus_route.rs
@@ -1,5 +1,5 @@
 use bus_system::data;
-use bus_system::location::{BusLocation, Location};
+use bus_system::location::{Location, PassengerBusLocation};
 use bus_system::passenger::{Passenger, PassengerOnboardingBusSchedule};
 use bus_system::{
     calculate_passenger_schedule_for_bus, convert_bus_route_list_to_passenger_bus_route_list,
@@ -7,38 +7,100 @@ use bus_system::{
 use std::path::Path;
 use uuid::uuid;
 
-#[test]
-fn can_find_basic_route() {
-    let input_data =
-        data::read_data_from_file(Path::new("./tests/test_data/simple_data.json")).unwrap();
+fn get_passenger_bus_routes_from_input_data(
+    input_file: &Path,
+) -> (Vec<Vec<PassengerBusLocation>>, Vec<Location>) {
+    let full_path = Path::new("./tests/test_data/").join(input_file);
+    let full_path = full_path.as_ref();
+    let input_data = data::read_data_from_file(full_path).unwrap();
     let bus_routes = input_data.bus_routes;
-    let passenger_facing_bus_routes = bus_routes
+    let passenger_facing_bus_route = bus_routes
         .into_iter()
         .map(convert_bus_route_list_to_passenger_bus_route_list)
         .collect::<Vec<_>>();
-    let test_passenger: Passenger = Into::<Passenger>::into(input_data.passengers[0].clone());
+    (passenger_facing_bus_route, input_data.location_vector)
+}
+
+#[test]
+fn can_find_basic_route() {
+    let (passenger_facing_bus_routes, location_list) =
+        get_passenger_bus_routes_from_input_data(Path::new("simple_data.json"));
+    let test_passenger: Passenger = Passenger::new(location_list[3], location_list[2]);
     let calculated_passenger_bus_route =
         calculate_passenger_schedule_for_bus(test_passenger, 0, &passenger_facing_bus_routes);
-    let expected_passenger_bus_route = [
+    let expected_passenger_bus_route = vec![
         PassengerOnboardingBusSchedule {
             time_tick: 4,
-            bus_num: Some(2),
-            stop_location: Location {
-                index: 1,
-                id: uuid!("0adf8f60-3748-4af0-a985-5eb0de081d70"),
-            },
+            bus_num: Some(0),
+            stop_location: location_list[3],
         },
         PassengerOnboardingBusSchedule {
             time_tick: 8,
             bus_num: None,
-            stop_location: Location {
-                index: 3,
-                id: uuid!("135f70d1-0f07-4a0f-a996-3412019ea584"),
-            },
+            stop_location: location_list[2],
         },
     ];
     assert_eq!(
-        calculated_passenger_bus_route.unwrap(),
-        expected_passenger_bus_route
+        calculated_passenger_bus_route,
+        Ok(expected_passenger_bus_route),
     );
+}
+
+#[test]
+fn can_find_bus_route_with_transfer() {
+    let (passenger_facing_bus_routes, location_list) =
+        get_passenger_bus_routes_from_input_data(Path::new("simple_data.json"));
+    let test_passenger = Passenger::new(location_list[0], location_list[2]);
+    let calculated_passenger_bus_route =
+        calculate_passenger_schedule_for_bus(test_passenger, 0, &passenger_facing_bus_routes);
+
+    let expected_passenger_route = vec![
+        PassengerOnboardingBusSchedule {
+            time_tick: 4,
+            bus_num: Some(1),
+            stop_location: location_list[0],
+        },
+        PassengerOnboardingBusSchedule {
+            time_tick: 8,
+            bus_num: Some(2),
+            stop_location: location_list[3],
+        },
+        PassengerOnboardingBusSchedule {
+            time_tick: 12,
+            bus_num: None,
+            stop_location: location_list[2],
+        },
+    ];
+
+    assert_eq!(calculated_passenger_bus_route, Ok(expected_passenger_route));
+}
+
+#[test]
+fn finds_shortest_route() {
+    let (passenger_facing_bus_routes, location_list) =
+        get_passenger_bus_routes_from_input_data(Path::new("data_with_distances.json"));
+    let test_passenger = Passenger::new(location_list[1], location_list[2]);
+    let calculated_passenger_bus_route =
+        calculate_passenger_schedule_for_bus(test_passenger, 0, &passenger_facing_bus_routes);
+
+    let expected_passenger_route = vec![
+        PassengerOnboardingBusSchedule {
+            time_tick: 6,
+            bus_num: Some(2),
+            stop_location: location_list[1],
+        },
+        PassengerOnboardingBusSchedule {
+            // time tick represents time tick when bus 3 drops the passenger off, rather than when  bus 0 picks passenger up
+            time_tick: 14,
+            bus_num: Some(0),
+            stop_location: location_list[3],
+        },
+        PassengerOnboardingBusSchedule {
+            time_tick: 22,
+            bus_num: None,
+            stop_location: location_list[2],
+        },
+    ];
+
+    assert_eq!(calculated_passenger_bus_route, Ok(expected_passenger_route),);
 }
